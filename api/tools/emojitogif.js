93addcd90dcdef17dc935a9df4bef2a18d9f669d
Wahyu Store <wahyustoreid.com@gmail.com>
2025-06-04 18:09:49 +0700

    Add files via upload

---
+++ api/tools/emojitogif.js
@@ -0,0 +1,21 @@
+function encodeEmoji(emoji) {
+return [...emoji].map(char => char.codePointAt(0).toString(16)).join('');
+}
+
+module.exports = function(app) {
+app.get('/tools/emojitogif', async (req, res) => {
+const { apikey, emoji } = req.query
+if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
+const unik = await encodeEmoji(emoji)
+const image = await getBuffer(`https://fonts.gstatic.com/s/e/notoemoji/latest/${unik}/512.webp`)
+        try {
+            res.writeHead(200, {
+                'Content-Type': 'image/png',
+                'Content-Length': image.length,
+            });
+            res.end(image);
+        } catch (error) {
+            res.status(500).send(`Error: ${error.message}`);
+        }
+});
+}
---
+++ api/tools/ssweb.js
@@ -0,0 +1,16 @@
+module.exports = function(app) {
+app.get('/tools/ssweb', async (req, res) => {
+       const { apikey, url } = req.query
+       if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
+       if (!url) return res.json({ status: false, error: 'Url is required' });
+        try {
+            let anu = await fetchJson(`https://api.pikwy.com/?tkn=125&d=3000&u=${url}&fs=0&w=1280&h=1200&s=100&z=100&f=$jpg&rt=jweb`)
+            res.status(200).json({
+                status: true,
+                result: anu.iurl
+            });
+        } catch (error) {
+            res.status(500).send(`Error: ${error.message}`);
+        }
+});
+}
